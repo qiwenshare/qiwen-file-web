d5b1a73cee553b76b364b072734b0e108d35e654
liyating <2435065796@qq.com>
2020-04-21 11:06:53 +0800

    底部样式调整

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -6,9 +6,7 @@
         <img class="logo" :src="logoUrl" />
         <div class="link-wrapper">
           <div class="info-website">地址：陕西省西安市雁塔区</div>
-          <div class="info-email">
-            邮箱：1162714483@qq.com &nbsp; 2435065796@qq.com
-          </div>
+          <div class="info-email">邮箱：1162714483@qq.com &nbsp; 2435065796@qq.com</div>
           <div class="info-welcome">如果你也是对技术感兴趣的小伙伴，欢迎加入我们</div>
         </div>
       </div>
@@ -26,11 +24,11 @@
     <div class="copy-right-wrapper">
       <div class="copy-right">
         <span>奇文共赏 2020 版权所有&nbsp;|&nbsp;</span>
-        <span><a style="color:white;" href="http://beian.miit.gov.cn/" target="_blank">陕ICP备19020251号</a>&nbsp;</span>        
+        <span>
+          <a style="color:white;" href="http://beian.miit.gov.cn/" target="_blank">陕ICP备19020251号</a>&nbsp;
+        </span>
       </div>
-      <div class="tip-website">
-        为获得最佳浏览体验，建议使用IE11、FireFox50.5、Chrome51.0及以上版本的浏览器
-      </div>
+      <div class="tip-website">为获得最佳浏览体验，建议使用IE11、FireFox50.5、Chrome51.0及以上版本的浏览器</div>
     </div>
   </div>
 </template>
@@ -83,65 +81,65 @@ export default {
 <style lang="stylus" scoped>
 @import '~@/assets/styles/varibles.styl'
 .footer-wrapper
-  background: $Info
-  margin-top: 20px
+  background $Info
+  margin-top 20px
   >>> .divider
-    height: 3px
-    margin: 0
+    height 3px
+    margin 0
   .footer-top
-    width: 80%
-    margin: 0 auto
-    padding: 40px 0
-    display: flex
-    justify-content: space-between
+    width 90%
+    margin 0 auto
+    padding 40px 0
+    display flex
+    justify-content space-between
     .info
-      display: flex
-      width: 75%
+      display flex
+      width 75%
       .logo
-        height: 130px
+        height 126px
       .link-wrapper
-        color: $BorderLight
-        flex: 1
-        padding: 20px 10px 10px 40px
-        line-height: 40px
+        color $BorderLight
+        flex 1
+        padding 20px 10px 10px 40px
+        line-height 40px
         .info-website
           .phone
             .phone-number
-              font-size: 28px
-              padding-bottom: 4px
+              font-size 28px
+              padding-bottom 4px
             .phone-text
-              text-align: center
+              text-align center
     .code-2-img-wrapper
-      width: 25%
+      width 25%
       padding-top 20px
-      display: flex
+      display flex
+      justify-content space-around
       .code-info
-        width: 36%;
-        text-align: center;
-        margin-right: 40px;
+        width 36%
+        text-align center
         .code-2-img
-          width: 90%
-          height: auto
-          margin-bottom: 10px
+          width 90%
+          height auto
+          margin-bottom 10px
         .code-2-text
-          width: 90%
-          text-align: center
-          color: $BorderLight
-          margin: 0 auto
+          width 90%
+          text-align center
+          color $BorderLight
+          margin 0 auto
   .footer-contact
-    color: $BorderLight
+    color $BorderLight
     a
-      color: #a5aaa9
+      color #a5aaa9
     .contact-top
-      padding-top: 20px
-      line-height: 30px
+      padding-top 20px
+      line-height 30px
     .contact-bottom
-      line-height: 35px
+      line-height 35px
   .copy-right-wrapper
-    color: $BorderLight
-    background: $Primary
-    text-align: center
-    padding: 20px 0
-    font-size: 14px
-    color: $BorderLight
+    color $BorderLight
+    background $Primary
+    text-align center
+    padding 20px 0
+    font-size 14px
+    color $BorderLight
 </style>
