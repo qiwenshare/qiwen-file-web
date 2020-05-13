7daa73a16ba5aa4c757ebe0ddc700ba76396defc
liyating <2435065796@qq.com>
2020-05-13 16:38:48 +0800

    1280及以下分辨率操作列默认收缩；1280以上分辨率操作列默认展开

--- src/store.js
+++ src/store.js
@@ -11,24 +11,27 @@ export default new Vuex.Store({
     userId: 0,
     userImgUrl: '',
     userInfoObj: {},
-    operaColumnExpand: Number(sessionStorage.getItem('operaColumnExpand')), //  操作列是否展开，0不展开，1展开
+    operaColumnExpand: 
+      sessionStorage.getItem('operaColumnExpand') ? 
+        Number(sessionStorage.getItem('operaColumnExpand')) : 
+        (document.body.clientWidth > 1280 ? 1 : 0), //  操作列是否展开，0不展开，1展开
     isFolder: Number(sessionStorage.getItem('isFolder')), //  左侧栏是否折叠，0不折叠，1折叠
-    selectedColumnList: sessionStorage.getItem('selectedColumnList') ? sessionStorage.getItem('selectedColumnList').split(",") : ['extendname','filesize','uploadtime'],  //  列显隐
+    selectedColumnList: sessionStorage.getItem('selectedColumnList') ? sessionStorage.getItem('selectedColumnList').split(",") : ['extendname', 'filesize', 'uploadtime'],  //  列显隐
     imageModel: Number(sessionStorage.getItem('imageModel'))  //  图片类型页面是否展示为网格模式，0不是，1是
   },
   mutations: {
     changeLogin(state, data) { // 改变state中的状态值
       state.isLogin = data;
     },
-    changeUsername(state,data) {
+    changeUsername(state, data) {
       state.username = data;
     },
     changeUserId(state, data) {
       state.userId = data;
     },
     changeUserImgUrl(state, data) {
-      if(data) {
-        if(data.includes('http')) { //  若是QQ头像
+      if (data) {
+        if (data.includes('http')) { //  若是QQ头像
           state.userImgUrl = data
         } else {
           state.userImgUrl = '/api' + data.split(".")[0] + "_min." + data.split(".")[1]
@@ -37,22 +40,22 @@ export default new Vuex.Store({
         state.userImgUrl = '/api'
       }
     },
-    changeUserInfoObj(state,data) {
-      state.userInfoObj = Object.assign({},state.userInfoObj,data)
+    changeUserInfoObj(state, data) {
+      state.userInfoObj = Object.assign({}, state.userInfoObj, data)
     },
-    changeOperaColumnExpand(state,data) {
+    changeOperaColumnExpand(state, data) {
       sessionStorage.setItem('operaColumnExpand', data)
       state.operaColumnExpand = data
     },
-    changeIsFolder(state,data) {
+    changeIsFolder(state, data) {
       sessionStorage.setItem('isFolder', data)
       state.isFolder = data
     },
-    changeSelectedColumnList(state,data) {
+    changeSelectedColumnList(state, data) {
       sessionStorage.setItem('selectedColumnList', data)
       state.selectedColumnList = data
     },
-    changeImageModel(state,data) {
+    changeImageModel(state, data) {
       sessionStorage.setItem('imageModel', data)
       state.imageModel = data
     }
@@ -60,12 +63,12 @@ export default new Vuex.Store({
   actions: {
     getUserInfo(context) {
       return checkUserLoginInfo().then(res => {
-        if(res.success) {
+        if (res.success) {
           context.commit("changeLogin", res.success)
           context.commit("changeUsername", res.data.username)
           context.commit("changeUserId", res.data.userId)
-          context.commit('changeUserImgUrl',res.data.imageurl);
-          context.commit('changeUserInfoObj',res.data)
+          context.commit('changeUserImgUrl', res.data.imageurl);
+          context.commit('changeUserInfoObj', res.data)
         } else {
           context.commit("changeLogin", res.success)
         }
