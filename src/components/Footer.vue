80a3e41ebb426de9283c1b84282f6da0fca228ed
liyating <2435065796@qq.com>
2021-02-22 23:56:44 +0800

    底部栏优化，和社区页面底部栏保持一致

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,78 +1,61 @@
 <template>
   <div class="footer-wrapper">
-    <ScpDivider></ScpDivider>
-    <div class="footer-top">
-      <div class="info">
-        <img class="logo" :src="logoUrl" />
-        <div class="link-wrapper">
-          <div class="info-website">地址：陕西省西安市雁塔区</div>
-          <div class="info-email">邮箱：1162714483@qq.com &nbsp; 2435065796@qq.com</div>
-          <div class="info-welcome">如果你也是对技术感兴趣的小伙伴，欢迎加入我们</div>
-        </div>
-      </div>
-      <div class="code-2-img-wrapper">
-        <div class="code-info">
-          <img class="code-2-img" :src="QQImg" />
-          <p class="code-2-text">QQ群</p>
-        </div>
-        <div class="code-info">
-          <img class="code-2-img" :src="wechatImg" />
-          <p class="code-2-text">微信公众号</p>
-        </div>
+    <div class="copy-right-wrapper">
+      <img class="logo" :src="logoUrl" :alt="$store.getters.imgAlt + 'footerLogo'" />
+      <div class="copy-right">
+        <span>奇文共赏 2020 版权所有</span>
+        <span class="split">|</span>
+        <span
+          ><a class="link" style="color:white;" href="http://beian.miit.gov.cn/" target="_blank">陕ICP备19020251号</a>&nbsp;</span
+        >
+        <p class="tip-website">
+          为获得最佳浏览体验，建议使用IE11、FireFox50.5、Chrome51.0及以上版本的浏览器
+        </p>
       </div>
     </div>
-    <div class="copy-right-wrapper">
-      <div class="copy-right">
-        <span>奇文共赏 2020 版权所有&nbsp;|&nbsp;</span>
-        <span>
-          <a style="color:white;" href="http://beian.miit.gov.cn/" target="_blank">陕ICP备19020251号</a>&nbsp;
-        </span>
-      </div>
-      <div class="tip-website">为获得最佳浏览体验，建议使用IE11、FireFox50.5、Chrome51.0及以上版本的浏览器</div>
+    <div class="join-us">
+      <div class="desc">如果你也是对技术感兴趣的小伙伴，欢迎加入我们</div>
+      <ul class="join-list">
+        <li class="join-item" v-for="(item, index) in joinList" :key="index">
+          <el-popover
+            placement="top"
+            trigger="hover"
+          >
+            <img class="img" :src="item.img" :alt="`${$store.getters.imgAlt} ${item.title}`" style="width: 130px;">
+            <i slot="reference" :class="`iconfont ${item.logo} ${item.class}`"></i>
+          </el-popover>
+        </li>
+      </ul>
     </div>
   </div>
 </template>
 
 <script>
-import ScpDivider from '@/components/ScpDivider'
-
 export default {
   name: 'Footer',
-  components: {
-    ScpDivider
-  },
   data() {
     return {
       logoUrl: require('@/assets/images/common/logo_footer.png'),
-      footerLinkList: [
+      joinList: [
         {
-          link: 'javascript:;',
-          text: '友情链接：'
+          class: 'qq',
+          logo: 'icon-qq',
+          img: require('@/assets/images/footer/QQImg.png'),
+          title: 'QQ群'
         },
         {
-          link: 'https://gitee.com/',
-          text: '码云'
+          class: 'wechat',
+          logo: 'icon-weixin-copy',
+          img: require('@/assets/images/footer/wechatImg.png'),
+          title: '微信公众号'
         },
         {
-          link: 'https://www.runoob.com/',
-          text: '菜鸟教程'
-        },
-        {
-          link: 'https://element.eleme.cn/#/zh-CN/component/installation',
-          text: 'Element'
-        },
-        {
-          link:
-            'https://docs.spring.io/spring-boot/docs/2.1.9.RELEASE/reference/html/',
-          text: 'Spring Boot'
-        },
-        {
-          link: 'https://cn.vuejs.org/',
-          text: 'Vue.js'
+          class: 'gitee',
+          logo: 'icon-mayun',
+          img: require('@/assets/images/footer/giteeImg.png'),
+          title: '码云 开源社区'
         }
-      ],
-      QQImg: require('@/assets/images/footer/QQImg.png'),
-      wechatImg: require('@/assets/images/footer/wechatImg.png')
+      ]
     }
   }
 }
@@ -80,66 +63,49 @@ export default {
 
 <style lang="stylus" scoped>
 @import '~@/assets/styles/varibles.styl'
+
 .footer-wrapper
-  background $Info
-  margin-top 20px
-  >>> .divider
-    height 3px
-    margin 0
-  .footer-top
-    width 90%
-    margin 0 auto
-    padding 40px 0
-    display flex
-    justify-content space-between
-    .info
-      display flex
-      width 75%
-      .logo
-        height 126px
-      .link-wrapper
-        color $BorderLight
-        flex 1
-        padding 20px 10px 10px 40px
-        line-height 40px
-        .info-website
-          .phone
-            .phone-number
-              font-size 28px
-              padding-bottom 4px
-            .phone-text
-              text-align center
-    .code-2-img-wrapper
-      width 25%
-      padding-top 20px
-      display flex
-      justify-content space-around
-      .code-info
-        width 36%
-        text-align center
-        .code-2-img
-          width 90%
-          height auto
-          margin-bottom 10px
-        .code-2-text
-          width 90%
-          text-align center
-          color $BorderLight
-          margin 0 auto
-  .footer-contact
-    color $BorderLight
-    a
-      color #a5aaa9
-    .contact-top
-      padding-top 20px
-      line-height 30px
-    .contact-bottom
-      line-height 35px
+  margin-top: 20px
+  display flex
   .copy-right-wrapper
-    color $BorderLight
-    background $Primary
-    text-align center
-    padding 20px 0
-    font-size 14px
-    color $BorderLight
+    width 65%
+    color: $BorderLight
+    background linear-gradient(to right, $Primary, #66b1ff)
+    padding: 16px 0 16px 5vw
+    font-size: 14px
+    color: $BorderLight
+    display flex
+    align-items center
+    .logo
+      width 240px
+      display block
+    .split
+      margin 0 8px
+    .link
+      &:hover
+        text-decoration underline
+    .tip-website
+      padding-top 8px
+  .join-us
+    background linear-gradient(to right, $Placeholder, $Info)
+    width: 35%
+    padding: 16px 5vw 16px 40px
+    color: #fff
+    .join-list
+      margin-top 16px
+      display flex
+      .join-item
+        .iconfont
+          margin-right 16px
+          font-size 30px
+          cursor: pointer;
+        .qq
+          &:hover
+            color $Primary
+        .wechat
+          &:hover
+            color $Success
+        .gitee
+          &:hover
+            color #c71d24
 </style>
