d75d44e3d42d3b7e8cbc690f3c49eb9a52d756dc
liyating <2435065796@qq.com>
2021-09-09 11:18:13 +0800

    🎈 perf(Axios & router): Axios & router 封装优化
    完善 Axios 响应拦截器异常情况处理；vue-router 全局路由前置守卫对无需登录权限校验的路由放开权限校验

--- src/request/http.js
+++ src/request/http.js
@@ -1,19 +1,19 @@
 import axios from 'axios'
 import globalFunction from '@/globalFunction.js'
 import router from '@/router/router'
-import { MessageBox } from 'element-ui';
+import { MessageBox, Message } from 'element-ui'
 
 // 登录提醒
-const loginTip = function() {
-  MessageBox.alert('您尚未登录，请先登录', '操作提示', {
-    confirmButtonText: '确定',
-    callback: () => {
-      router.push({
-        path: '/login',
-        query: { Rurl: router.currentRoute.fullPath }  //  将当前页面的url传递给login页面进行操作
-      })
-    }
-  });
+const loginTip = function () {
+	MessageBox.alert('您尚未登录，请先登录', '操作提示', {
+		confirmButtonText: '确定',
+		callback: () => {
+			router.push({
+				path: '/login',
+				query: { Rurl: router.currentRoute.fullPath } //  将当前页面的url传递给login页面进行操作
+			})
+		}
+	})
 }
 
 // 请求超时时间
@@ -23,43 +23,53 @@ axios.defaults.timeout = 10000 * 5
 axios.defaults.baseURL = '/api'
 
 // POST 请求头
-axios.defaults.headers.post['Content-Type'] = 'application/x-www-form-urlencoded'
+axios.defaults.headers.post['Content-Type'] =
+	'application/x-www-form-urlencoded'
 
 // 请求携带cookie
 axios.defaults.withCredentials = true
 
 // 请求拦截器
 axios.interceptors.request.use(
-  (config) => {
-    config.headers['token'] = globalFunction.getCookies('token')
-    return config
-  },
-  (error) => {
-    console.log(error)
-    return Promise.reject(error)
-  }
+	(config) => {
+		config.headers['token'] = globalFunction.getCookies('token')
+		return config
+	},
+	(error) => {
+		console.log(error)
+		return Promise.reject(error)
+	}
 )
 
 // 响应拦截器
 axios.interceptors.response.use(
-  (response) => {
-    if (response.status === 200) {
-      return Promise.resolve(response)
-    }
-  },
-  // 服务器状态码不是200的情况
-  (error) => {
-    if (error.response.status) {
-      console.log(error.response)
-      switch(error.response.status) {
-        case 401:
-          loginTip()
-          break
-        default:
-          return Promise.reject(error.response)
-      }
-    }
-  }
+	(response) => {
+		if (response.status === 200) {
+			return Promise.resolve(response)
+		}
+	},
+	// 服务器状态码不是200的情况
+	(error) => {
+		if (error.response.status) {
+			console.log(error.response)
+			switch (error.response.status) {
+				case 404:
+					Message.warning('接口不存在，请刷新重试或联系管理员')
+					break
+				case 401:
+					loginTip()
+					break
+				case 500:
+					Message.error('服务异常，请稍后刷新重试或联系管理员')
+					break
+				case 502:
+					Message.error('服务异常，请稍后刷新重试或联系管理员')
+					break
+				default:
+					return Promise.reject(error.response)
+			}
+		}
+	}
 )
 
 /**
@@ -69,18 +79,18 @@ axios.interceptors.response.use(
  * @returns {Promise}
  */
 export function get(url, params) {
-  return new Promise((resolve, reject) => {
-    axios
-      .get(url, {
-        params: params
-      })
-      .then((res) => {
-        resolve(res.data)
-      })
-      .catch((err) => {
-        reject(err.data)
-      })
-  })
+	return new Promise((resolve, reject) => {
+		axios
+			.get(url, {
+				params: params
+			})
+			.then((res) => {
+				resolve(res.data)
+			})
+			.catch((err) => {
+				reject(err.data)
+			})
+	})
 }
 /**
  * 封装 post 方法，对应 POST 请求
@@ -90,24 +100,24 @@ export function get(url, params) {
  * @returns {Promise}
  */
 export function post(url, data = {}, info) {
-  return new Promise((resolve, reject) => {
-    let newData = data
-    if (info) {
-      //  转formData格式
-      newData = new FormData()
-      for (let i in data) {
-        newData.append(i, data[i])
-      }
-    }
-    axios
-      .post(url, newData)
-      .then((res) => {
-        resolve(res.data)
-      })
-      .catch((err) => {
-        reject(err.data)
-      })
-  })
+	return new Promise((resolve, reject) => {
+		let newData = data
+		if (info) {
+			//  转formData格式
+			newData = new FormData()
+			for (let i in data) {
+				newData.append(i, data[i])
+			}
+		}
+		axios
+			.post(url, newData)
+			.then((res) => {
+				resolve(res.data)
+			})
+			.catch((err) => {
+				reject(err.data)
+			})
+	})
 }
 
 /**
@@ -117,16 +127,16 @@ export function post(url, data = {}, info) {
  * @returns {Promise}
  */
 export function put(url, params = {}) {
-  return new Promise((resolve, reject) => {
-    axios.put(url, params).then(
-      (res) => {
-        resolve(res.data)
-      },
-      (err) => {
-        reject(err.data)
-      }
-    )
-  })
+	return new Promise((resolve, reject) => {
+		axios.put(url, params).then(
+			(res) => {
+				resolve(res.data)
+			},
+			(err) => {
+				reject(err.data)
+			}
+		)
+	})
 }
 
 /**
@@ -136,14 +146,14 @@ export function put(url, params = {}) {
  * @returns {Promise}
  */
 export function axiosDelete(url, params = {}) {
-  return new Promise((resolve, reject) => {
-    axios
-      .delete(url, params)
-      .then((res) => {
-        resolve(res.data)
-      })
-      .catch((err) => {
-        reject(err.data)
-      })
-  })
+	return new Promise((resolve, reject) => {
+		axios
+			.delete(url, params)
+			.then((res) => {
+				resolve(res.data)
+			})
+			.catch((err) => {
+				reject(err.data)
+			})
+	})
 }
