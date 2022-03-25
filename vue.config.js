54ac2319866e5a391875e52976cae63b89da008b
liyating <2435065796@qq.com>
2022-03-25 14:03:56 +0800

    🐳 chore(启动配置): 指定前端工程启动端口

--- vue.config.js
+++ vue.config.js
@@ -4,13 +4,15 @@ module.exports = {
 	// 选项...
 	publicPath: '/',
 
+	// 配置项不懂的地方，请查看 Vue CLI 官方文档 https://cli.vuejs.org/zh/config/#devserver
 	devServer: {
 		disableHostCheck: true,
 		host: '0.0.0.0',
+		port: 8081, //  本地开发环境 - 前端工程启动的端口；如果不想指定端口，期望启动追加，请注释 port 属性
 		// 配置代理，解决本地开发环境下跨域请求后台接口的问题，proxy 中的修改项修改完后需要重启项目才可生效
 		proxy: {
 			'/api': {
-				target: 'http://localhost:8080', //  本地开发环境 - 连接后台接口
+				target: 'http://localhost:8080', //  本地开发环境 - 连接后台服务，协议 + IP + 端口
 				ws: true, //是否跨域
 				changeOrigin: true,
 				pathRewrite: {
